490a87810b77430ab676cebace2c5b6d79a0183f
ShahzodZiyodullayev <sshahzod5@gmail.com>
2023-06-06 02:00:28 +0500

    ++

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import { StyledEngineProvider, CssBaseline } from "@mui/material";
 import AuthProvider from "./Auth/AuthProvider";
 import Routes from "./routes";
 import SnackbarComponent from "./components/SnackbarComponent";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function App() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Comfortaa, sans-serif", // Comforta yazı tipi veya başka bir yazı tipi adı
+    },
+  });
+
   return (
     <StyledEngineProvider injectFirst>
       <CssBaseline />
       <AuthProvider>
-        <Routes />
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
         <SnackbarComponent />
       </AuthProvider>
     </StyledEngineProvider>
